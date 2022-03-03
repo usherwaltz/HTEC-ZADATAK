b834bc95e82224972a77f62d7328967c33d6bcfe
usherwaltz <usherwalce@gmail.com>
2022-03-03 04:02:22 +0100

    If applied, this commit will implement the fetchPlayers by tournament ID API and integrated in the tournament_details_page.dart screen.

--- lib/helper_widgets/appbar_default.dart
+++ lib/helper_widgets/appbar_default.dart
@@ -9,6 +9,7 @@ class AppBarDefault extends StatelessWidget with PreferredSizeWidget {
   final List<Widget> actions;
   String title;
   final titleStyle;
+  final leading;
   var tourId;
 
   AppBarDefault(
@@ -17,6 +18,7 @@ class AppBarDefault extends StatelessWidget with PreferredSizeWidget {
         this.actions = const [],
         this.title = '',
         this.titleStyle,
+        this.leading,
         this.tourId
       }) : preferredSize = const Size.fromHeight(50.0),
         super(key: key);
@@ -36,6 +38,7 @@ class AppBarDefault extends StatelessWidget with PreferredSizeWidget {
       title: Text(
         title
       ),
+      leading: leading,
       centerTitle: centerTitle,
       actions: actions,
       automaticallyImplyLeading: true,
--- lib/screens/tournament_details_page.dart
+++ lib/screens/tournament_details_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import '../../models/players_model.dart';
 import '../../helper_widgets/layout.dart';
 import '../../helper_classes/page_appbars.dart';
 
@@ -15,13 +19,107 @@ class TournamentDetailsPage extends StatefulWidget {
 }
 
 class _TournamentDetailsPageState extends State<TournamentDetailsPage> {
+
+  late Future futurePlayers;
+
+  Future fetchPlayersList() async {
+    const url = 'http://internships-mobile.htec.co.rs/api/players';
+    final response = await http.get(Uri.parse(url), headers: {"x-tournament-id":"${widget.tournamentId}"});
+
+    if(response.statusCode == 200) {
+      var decodedResponse = jsonDecode(response.body);
+      var parsed = decodedResponse['data'];
+      List<Players> players = convertToPlayerModel(parsed);
+
+      players.sort((b, a) => a.points.compareTo(b.points));
+      return players;
+    } else {
+      throw Exception("Failed to load data");
+    }
+  }
+
+  convertToPlayerModel(List responseData) {
+    return List<Players>.from(responseData.map((i) => Players.fromJson(i)));
+  }
+
+  // sortPlayersByPoints(players) {
+  //   var newMap = Map.fromEntries(originalMap.entries.toList()..sort((e1, e2) =>
+  //       int.parse(e1.value["SortOrder"]).compareTo(int.parse(e2.value["SortOrder"]))));
+  //   players as List;
+  //   players.sort((a, b) => a.points.compareTo(b.points));
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    print('new State');
+    futurePlayers = fetchPlayersList();
+  }
+
   @override
   Widget build(BuildContext context) {
     var appbar = PageAppBars.tournamentDetailsPage;
     appbar.tournamentId = widget.tournamentId;
     return Layout(
       appBar: appbar,
-      body: Container()
+      body: FutureBuilder(
+       future: futurePlayers,
+        builder: (context, AsyncSnapshot snapshot) {
+         if(snapshot.hasData) {
+           List<dynamic> playersList = snapshot.data;
+           return ListView.separated(
+             separatorBuilder: (context, index) {
+               return const Divider(height: 10);
+             },
+             padding: const EdgeInsets.all(20.0),
+             itemCount: playersList.length,
+             itemBuilder: (context, index) {
+               Players player = playersList[index];
+               return GestureDetector(
+                 onTap: () {},
+                 child: SizedBox(
+                   height: 150,
+                   child: Card(
+                     elevation: 10,
+                     child: Padding(
+                       padding: const EdgeInsets.only(left: 20, right: 20),
+                       child: Column(
+                         crossAxisAlignment: CrossAxisAlignment.start,
+                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                         children: [
+                           Text(
+                             "${player.firstName} ${player.lastName}",
+                             style: const TextStyle(
+                               fontSize: 20
+                             ),
+                           ),
+                           Text(
+                               'Points: ${player.points}'
+                           ),
+                           Text(
+                               player.isProfessional == 1 ?
+                                 "Professional: Yes" :
+                                 "Professional: No"
+                           ),
+                           if(player.tournamentId)
+                             Text("Tournament ID: ${player.tournamentId}")
+                         ],
+                       ),
+                     ),
+                   ),
+                 ),
+               );
+             },
+           );
+         } else if (snapshot.hasError) {
+           return Text("${snapshot.error}");
+         }
+
+         return const Center(
+           child: CircularProgressIndicator()
+         );
+        },
+      )
     );
   }
 }
